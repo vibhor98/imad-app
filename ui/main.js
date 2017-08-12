02607229c1facc19b2266ccac7dbd62921576540
Vibhor Agarwal <agarwalvibhor84@gmail.com>
2017-08-12 09:42:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,15 +22,26 @@ img.onclick = function() {
 };*/
 
 //Counter code
-var counter = 0;
+
 var button = document.getElementById('counter');
 button.onclick = function() {
-  //make a request to the counter endpoint
+  
+  //create a request obj
+  var request = new XMLHttpRequest();
   
   //capture the response and store it in var
-  
-  //render the var in the correct span 
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+  request.onreadystatechange = function() {
+      if(request.readyState === XMLHttpRequest.DONE) {
+          //take some action
+          if(request.status === 200) {
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+          }
+      }
+      //else do nothing
+  }; 
+  //make a request to the counter endpoint
+  request.open('GET', 'http://agarwalvibhir84.imad.hasura-app.io/counter', true);
+  request.send(null);
 };
