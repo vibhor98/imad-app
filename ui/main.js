d8ae529813333a22fbcbac67b067e6f525660a31
Vibhor Agarwal <agarwalvibhor84@gmail.com>
2017-08-12 09:25:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 
 //change the text of main-text div
 var element = document.getElementById('main-text');
@@ -19,4 +19,18 @@ function moveRight() {
 var img = document.getElementById('madi');
 img.onclick = function() {
   var interval = setInterval(moveRight, 50);
+};*/
+
+//Counter code
+var counter = 0;
+var button = document.getElementById('counter');
+button.onclick = function() {
+  //make a request to the counter endpoint
+  
+  //capture the response and store it in var
+  
+  //render the var in the correct span 
+  counter = counter + 1;
+  var span = document.getElementById('counter');
+  span.innerHTML = counter.toString();
 };
