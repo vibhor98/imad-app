afb5f0c02fccf670f5cebea93ec2331fe0dfa931
Vibhor Agarwal <agarwalvibhor84@gmail.com>
2017-08-12 08:11:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,11 +12,11 @@ img.onclick = function() {
 
 var marginLeft = 0;
 function moveRight() {
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
 //move the image
 var img = document.getElementById('madi');
 img.onclick = function() {
-  var interval = setInterval(moveRight, 100);
+  var interval = setInterval(moveRight, 50);
 };
