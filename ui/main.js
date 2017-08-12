75d34312ed6cbced9e2082766b86c3ee4cef2edd
Vibhor Agarwal <agarwalvibhor84@gmail.com>
2017-08-12 10:31:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -51,15 +51,30 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit-btn');
 submit.onclick = function() {
+    
+    //create a request obj
+  var request = new XMLHttpRequest();
+  
+  //capture the response and store it in var
+  request.onreadystatechange = function() {
+      if(request.readyState === XMLHttpRequest.DONE) {
+          //take some action
+          if(request.status === 200) {
+              var names = request.responseText;
+              names = JSON.parse(names);
+              var list = '';
+              for(var i=0; i<names.length; i++) {
+                    list += '<li>' + names[i] + '</li>';
+              }
+              var ul = document.getElementById('namelist');
+              ul.innerHTML = list;
+          }
+      }
+  };
   //make a request to the server and send the name
+  request.open('GET', 'http://agarwalvibhor84.imad.hasura-app.io/submit-name?name=' + name, true);
+  request.send(null);
   //capture a list of names and render it as a list
-  var names = ['name1', 'name2', 'name3', 'name4'];
-  var list = '';
-  for(var i=0; i<names.length; i++) {
-      list += '<li>' + names[i] + '</li>';
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
 };
 
 
