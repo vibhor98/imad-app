739983eefe959aaac12c8758881ea5a1f0634a36
Vibhor Agarwal <agarwalvibhor84@gmail.com>
2017-08-13 15:03:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ function createTemplate(data) {
                 <hr/>
                 <h3>${heading}</h3>
                 <div>
-                    ${date}
+                    ${date.toDateString()}
                 </div>
                 <div>
                     ${content}
