8e3eade3acc14f6938cc9be0be41f5e87a35cb94
Vibhor Agarwal <agarwalvibhor84@gmail.com>
2017-08-13 15:23:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ app.get('/articles/:articleName', function(req, res) {
     // articleName == article-one
     //articles[articleName] == {} object for the articleOne
     
-    pool.query("SELECT * FROM article WHERE title = $1" + [req.params.articleName], function(err, result) {
+    pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function(err, result) {
         if(err) {
             res.status(500).send(err.toString());
         }  else {
