4e228bfd5baca7d48d20e2803de8da6688933ca1
Vibhor Agarwal <agarwalvibhor84@gmail.com>
2017-08-15 20:00:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,6 +127,11 @@ app.get('/check-login', function(req, res) {
         res.send('You are not logged in!');
 });
 
+app.get('/logout', function(req, res) {
+    delete req.session.auth;
+    res.send('Logged out!');
+});
+
 var pool = new Pool(config);
 app.get('/test-db', function(req, res) {
    //make a select request
