acb921ec806db2a0ca2fb02e333ad358ac69711c
Vibhor Agarwal <agarwalvibhor84@gmail.com>
2017-08-15 19:57:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,6 +106,11 @@ app.post('/login', function(req, res) {
                     var salt = dbString.split('$')[2];
                     var hashedPassword = hash( password, salt);
                     if(hashedPassword === dbString) {
+                        //set the session
+                        req.session.auth = {userId: result.rows[0].id};
+                        //set the cookie with session id
+                        //internally on server side, it maps the session id to an object
+                        //{auth : {userId}}
                         res.send('Credentials correct!');    
                     } else {
                         res.status(403).send('username/password is invalid.');
@@ -113,6 +118,13 @@ app.post('/login', function(req, res) {
                 }
         }
     });
+});
+
+app.get('/check-login', function(req, res) {
+    if(req.session && req.session.auth && req.session.auth.userId)
+        res.send("You're logged in: " + req.session.auth.userId.toString());
+    else
+        res.send('You are not logged in!');
 });
 
 var pool = new Pool(config);
