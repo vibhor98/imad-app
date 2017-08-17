c452af032af133516058b2711a42ada64f61e2c0
Vibhor Agarwal <agarwalvibhor84@gmail.com>
2017-08-18 00:10:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -77,6 +77,7 @@ submit.onclick = function() {
   request.setRequestHeader('Content-Type', 'application/json');
   request.send(JSON.stringify({username: username, password: password}));
   submit.value = 'Logging...'
+  loadArticles();
   //capture a list of names and render it as a list
 };
 
