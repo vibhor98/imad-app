6b00ad6c3d2bf0789151454a82eecace31590ab7
Vibhor Agarwal <agarwalvibhor84@gmail.com>
2017-08-20 09:34:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ function createTemplate(data) {
                     ${content}
                 </div>
             </div>
-            <div id="comments" >Comments:</div>
+            <div id="comments" ><center>Comments:</center></div>
             <script type="text/javascript" src="/ui/article.js"></script>
         </body>
     </html>
