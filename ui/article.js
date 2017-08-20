26760bf32112378e5bcf18564c980cf0cfd45863
Vibhor Agarwal <agarwalvibhor84@gmail.com>
2017-08-20 08:11:00 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -1,1 +1,21 @@
+var currentArticleTitle = window.location.pathname.split('/')[2];
 
+function loadCommentForm() {
+    
+}
+
+function loadlogin() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if(request.readystate === XMLHttpRequest.DONE) {
+            if(request.status === 200)
+                loadCommentForm(this.responseText);
+        }
+    };
+    request.open('GET', '/check-login', true);
+    request.send(null);
+}
+
+function loadcomments() {
+    
+}
